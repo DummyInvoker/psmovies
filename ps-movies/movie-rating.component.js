8b7fde50e85410518511f4e01ad8970e8fb7924f
DummyInvoker <zuoguainan@gmail.com>
2016-11-29 16:39:16 -0500

    Building a rating component

---
+++ ps-movies/movie-rating.component.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Sam.Sun on 11/29/2016.
+ */
+(function () {
+    "use strict";
+
+    var module = angular.module("psMovies");
+
+    module.component("movieRating", {
+        templateUrl: "ps-movies/movie-rating.component.html",
+        bindings: {
+            value: "<"
+        },
+        transclude: true,
+        controllerAs: "model",
+        controller: function () {
+            var model = this;
+
+            model.$onInit = function () {
+                model.entries = new Array(model.value);
+            };
+            
+            model.$onChanges = function () {
+                model.entries = new Array(model.value);
+            }
+        }
+    })
+}());
